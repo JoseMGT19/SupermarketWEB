ef25830ee251ced6317c81524afdb3928a08b8fa
jose manuel gonzales trochez <jgonzalez7243@campuscecep.edu.co>
2023-11-16 09:44:06 -0500

    PayModes Models,Pages

--- Models/PayMode.cs
+++ Models/PayMode.cs
@@ -5,8 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public int ProviderId { get; set; }
-
-        public Provider? Providers { get; set; } = default!;
+        public string? Observation { get; set; }
     }
 }
