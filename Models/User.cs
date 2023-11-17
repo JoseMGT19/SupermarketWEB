632b84f3523d0bb276c88cb27ed659532575d458
jose manuel gonzales trochez <jgonzalez7243@campuscecep.edu.co>
2023-11-17 15:13:07 -0500

    Creando el sistema de autenticacion

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketWEB.Models
+{
+    public class User
+    {
+        [Required] // Verificar que se importo using Ssytem.ComponentModel.DataAnnotations
+
+        [DataType(DataType.EmailAddress)]
+
+        public string Email { get; set; }
+        [Required]
+        [DataType (DataType.Password)]
+
+        public string Password { get; set; }    
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,12 @@ namespace SupermarketWEB
             // Add services to the container.
             builder.Services.AddRazorPages();
 
+            builder.Services.AddAuthentication().AddCookie("MyCookieAuth", options =>
+            {
+                options.Cookie.Name = "MyCookieAuth";
+                options.LoginPath = "/Account/Login";//Si no esta autenticado, cargue la pagina login
+            });
+
             //Agregando el contexto SupermarketContext a la aplicacion
             builder.Services.AddDbContext<SupermarketContext>(options => 
             options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB")));
